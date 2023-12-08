098f713dc6560882a59058783f1c78191355e8a3
Ilham Wahabi <wahabiputra@gmail.com>
2023-12-08 08:38:48 +0700

    Time: 73 ms (21.62%) | Memory: 47.9 MB (83.78%) - LeetSync

---
+++ 606-construct-string-from-binary-tree/construct-string-from-binary-tree.ts
@@ -0,0 +1,25 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+function tree2str(root: TreeNode | null): string {
+  if (!root) return ""
+
+  let concat = `${root.val}`
+  if (root.left) concat += `(${tree2str(root.left)})`
+  if (root.right) { 
+    concat += root.left ? `(${tree2str(root.right)})` : `()(${tree2str(root.right)})`
+  }
+
+  return concat
+};
