9aa36ed66d64242e2f9af58b6de37b2a0d5975c5
Ilham Wahabi <wahabiputra@gmail.com>
2023-12-08 09:07:18 +0700

    Time: 57 ms (49.25%) | Memory: 44.4 MB (46.88%) - LeetSync

---
+++ 94-binary-tree-inorder-traversal/binary-tree-inorder-traversal.ts
@@ -0,0 +1,29 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+function inorderTraversal(root: TreeNode | null): number[] {
+    const result = []
+
+    const traverse = (root: TreeNode | null): void => {
+        if (root) {
+            traverse(root.left)
+            result.push(root.val)
+            traverse(root.right)
+        }
+    }
+
+    traverse(root)
+
+    return result
+};
