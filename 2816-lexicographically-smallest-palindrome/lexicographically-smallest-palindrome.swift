54ef506d8928b6968955f6102f84c700aded3765
Ilham Wahabi <wahabiputra@gmail.com>
2023-12-10 06:05:55 +0700

    Time: 457 ms (30.30%) | Memory: 15 MB (93.94%) - LeetSync

---
+++ 2816-lexicographically-smallest-palindrome/lexicographically-smallest-palindrome.swift
@@ -0,0 +1,28 @@
+class Solution {
+    func makeSmallestPalindrome(_ s: String) -> String {
+        // create two pointers, a at start, b at end
+        // do while loop with a<=b
+        // check if the letter is same
+        // if not then determine the character number, the bigger got replaced by the smaller
+        // return modified string
+
+        var res = Array(s)
+        var a = 0
+        var b = res.count - 1
+
+        while (a <= b) {
+            if (res[a] < res[b]) {
+                print("a")
+                res[b] = res[a]
+            } else if (res[a] > res[b]) {
+                print("b")
+                res[a] = res[b]
+            }
+
+            a += 1
+            b -= 1
+        }
+
+        return String(res)
+    }
+}
