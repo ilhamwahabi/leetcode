e5d071d5f1fec2ff9b85070f0accf642b0c5f834
Ilham Wahabi <wahabiputra@gmail.com>
2023-12-09 08:18:16 +0700

    Time: 2 ms (75.61%) | Memory: 14.1 MB (28.66%) - LeetSync

---
+++ 94-binary-tree-inorder-traversal/binary-tree-inorder-traversal.swift
@@ -0,0 +1,32 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public var val: Int
+ *     public var left: TreeNode?
+ *     public var right: TreeNode?
+ *     public init() { self.val = 0; self.left = nil; self.right = nil; }
+ *     public init(_ val: Int) { self.val = val; self.left = nil; self.right = nil; }
+ *     public init(_ val: Int, _ left: TreeNode?, _ right: TreeNode?) {
+ *         self.val = val
+ *         self.left = left
+ *         self.right = right
+ *     }
+ * }
+ */
+class Solution {
+    func inorderTraversal(_ root: TreeNode?) -> [Int] {
+        var values: [Int] = []
+
+        func traverse(_ root: TreeNode?) {
+            if (root != nil) {
+                traverse(root!.left)
+                values.append(root!.val)
+                traverse(root!.right)
+            }
+        }
+
+        traverse(root)
+
+        return values
+    }
+}
