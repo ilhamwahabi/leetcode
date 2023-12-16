84aa717ad850361edeebdd1410f033bc0c3e230e
Ilham Wahabi <wahabiputra@gmail.com>
2023-12-17 05:37:50 +0700

    Time: 2 ms (86.96%) | Memory: 15 MB (8.70%) - LeetSync

---
+++ 2401-count-asterisks/count-asterisks.swift
@@ -0,0 +1,16 @@
+class Solution {
+    func countAsterisks(_ s: String) -> Int {
+        var parts = s.components(separatedBy: "|")
+
+        var count = 0
+        for i in stride(from: 0, to: parts.count, by: 2) {
+            for char in parts[i] {
+                if (char == "*") {
+                    count += 1
+                }
+            }
+        }
+
+        return count
+    }
+}
