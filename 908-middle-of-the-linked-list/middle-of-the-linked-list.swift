07807011f5a39fce7d28dadba84c1f41df9cbbca
Ilham Wahabi <wahabiputra@gmail.com>
2023-12-18 09:54:12 +0700

    Time: 2 ms (80.81%) | Memory: 14.2 MB (11.15%) - LeetSync

---
+++ 908-middle-of-the-linked-list/middle-of-the-linked-list.swift
@@ -0,0 +1,23 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public var val: Int
+ *     public var next: ListNode?
+ *     public init() { self.val = 0; self.next = nil; }
+ *     public init(_ val: Int) { self.val = val; self.next = nil; }
+ *     public init(_ val: Int, _ next: ListNode?) { self.val = val; self.next = next; }
+ * }
+ */
+class Solution {
+    func middleNode(_ head: ListNode?) -> ListNode? {
+        var arr: [ListNode] = []
+
+        var temp = head
+        while (temp != nil) {
+            arr.append(temp!)
+            temp = temp!.next
+        }
+
+        return arr[arr.count / 2]
+    }
+}
