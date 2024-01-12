7227c3d01de2e4ad6658f51c25bc8d4bc5cdfbaf
Ilham Wahabi <wahabiputra@gmail.com>
2024-01-12 12:13:11 +0700

    Time: 53 ms (88.57%) | Memory: 43.1 MB (97.14%) - LeetSync

--- 1823-determine-if-string-halves-are-alike/determine-if-string-halves-are-alike.ts
+++ 1823-determine-if-string-halves-are-alike/determine-if-string-halves-are-alike.ts
@@ -1,13 +1,12 @@
 function halvesAreAlike(s: string): boolean {
-    let vowel1 = 0
-    let vowel2 = 0
+    let count = 0
 
     const vowel = /[aeiouAEIOU]/
     const half = s.length / 2
     for (let i = 0; i < half; i++) {
-        if (vowel.test(s[i])) vowel1++
-        if (vowel.test(s[half + i])) vowel2++
+        if (vowel.test(s[i])) count++
+        if (vowel.test(s[half + i])) count--
     }
 
-    return vowel1 == vowel2
+    return count == 0
 };
