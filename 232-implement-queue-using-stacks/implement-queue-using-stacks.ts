8c0393c952fe9a86d027c0e0fe5f905a7f117def
Ilham Wahabi <wahabiputra@gmail.com>
2024-01-29 07:12:12 +0700

    Time: 57 ms (48.85%) | Memory: 49.6 MB (13.30%) - LeetSync

---
+++ 232-implement-queue-using-stacks/implement-queue-using-stacks.ts
@@ -0,0 +1,32 @@
+class MyQueue {
+    stack: number[] = []
+    
+    constructor() {
+        
+    }
+
+    push(x: number): void {
+        this.stack.push(x)
+    }
+
+    pop(): number {
+        return this.stack.shift()
+    }
+
+    peek(): number {
+        return this.stack[0]
+    }
+
+    empty(): boolean {
+        return this.stack.length === 0
+    }
+}
+
+/**
+ * Your MyQueue object will be instantiated and called as such:
+ * var obj = new MyQueue()
+ * obj.push(x)
+ * var param_2 = obj.pop()
+ * var param_3 = obj.peek()
+ * var param_4 = obj.empty()
+ */
