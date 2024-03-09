097eee003ed0a011a1d62e7e1269b358e72f3bc6
Ilham Wahabi <wahabiputra@gmail.com>
2024-03-09 10:39:19 +0700

    Time: 79 ms (45.00%) | Memory: 58.8 MB (90.00%) - LeetSync

---
+++ 2634-minimum-common-value/minimum-common-value.ts
@@ -0,0 +1,12 @@
+function getCommon(nums1: number[], nums2: number[]): number {
+    let i = 0
+    let j = 0
+
+    while (i < nums1.length && j < nums2.length) {
+        if (nums1[i] < nums2[j]) i++
+        else if (nums1[i] > nums2[j]) j++
+        else return nums1[i]
+    }
+
+    return -1
+};
