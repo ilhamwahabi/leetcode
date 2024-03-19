f5ea80dc738a41dba64592641e1ae285c165ebf6
Ilham Wahabi <wahabiputra@gmail.com>
2024-03-20 00:24:43 +0700

    Time: 65 ms (53.95%) | Memory: 52.1 MB (42.11%) - LeetSync

--- 777-toeplitz-matrix/toeplitz-matrix.ts
+++ 777-toeplitz-matrix/toeplitz-matrix.ts
@@ -1,9 +1,7 @@
 function isToeplitzMatrix(matrix: number[][]): boolean {
     for (let i = 0; i < matrix.length - 1; i++) {
         for (let j = 0; j < matrix[i].length - 1; j++) {
-            if (matrix[i+1][j+1] !== undefined && matrix[i][j] !== matrix[i+1][j+1]) {
-                return false
-            }
+            if (matrix[i][j] !== matrix[i+1][j+1]) return false
         }
     }
 
