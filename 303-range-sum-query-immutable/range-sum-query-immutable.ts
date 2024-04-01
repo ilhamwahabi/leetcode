f463691bc1de130a62b4d4b37e3a60a0d0b7876d
Ilham Wahabi <wahabiputra@gmail.com>
2024-04-02 00:21:59 +0700

    Time: 185 ms (22.16%) | Memory: 58.3 MB (86.59%) - LeetSync

---
+++ 303-range-sum-query-immutable/range-sum-query-immutable.ts
@@ -0,0 +1,23 @@
+class NumArray {
+    nums = []
+
+    constructor(nums: number[]) {
+        this.nums = nums
+    }
+
+    sumRange(left: number, right: number): number {
+        let result = 0
+
+        for (let i = left; i <= right; i++) {
+            result += this.nums[i]
+        }
+
+        return result
+    }
+}
+
+/**
+ * Your NumArray object will be instantiated and called as such:
+ * var obj = new NumArray(nums)
+ * var param_1 = obj.sumRange(left,right)
+ */
