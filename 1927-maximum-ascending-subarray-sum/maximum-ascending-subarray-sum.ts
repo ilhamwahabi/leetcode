f13ac295ad8966ec52300e50d6ca10bb8f2e953a
Ilham Wahabi <wahabiputra@gmail.com>
2024-04-03 04:25:17 +0700

    Time: 50 ms (86.36%) | Memory: 51 MB (86.36%) - LeetSync

---
+++ 1927-maximum-ascending-subarray-sum/maximum-ascending-subarray-sum.ts
@@ -0,0 +1,16 @@
+function maxAscendingSum(nums: number[]): number {
+    let max = nums[0]
+    let sum = nums[0]
+
+    for (let i = 1; i < nums.length; i++) {
+        if (nums[i] > nums[i - 1]) {
+            sum += nums[i]
+        } else {
+            sum = nums[i]
+        }
+
+        if (sum > max) max = sum
+    }
+
+    return max
+};
