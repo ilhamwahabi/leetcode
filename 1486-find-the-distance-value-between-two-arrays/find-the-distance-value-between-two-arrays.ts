158071b989010326e72281b958c2ea5d79c99d58
Ilham Wahabi <wahabiputra@gmail.com>
2024-04-02 12:10:43 +0700

    Time: 55 ms (84.62%) | Memory: 51.3 MB (96.15%) - LeetSync

---
+++ 1486-find-the-distance-value-between-two-arrays/find-the-distance-value-between-two-arrays.ts
@@ -0,0 +1,18 @@
+function findTheDistanceValue(arr1: number[], arr2: number[], d: number): number {
+    let count = 0
+    
+    for (let i = 0; i < arr1.length; i++) {
+        let isValid = true
+        
+        for (let j = 0; j < arr2.length; j++) {
+            if (Math.abs(arr1[i] - arr2[j]) <= d) {
+                isValid = false
+                break
+            }
+        }
+
+        if (isValid) count++
+    }
+
+    return count
+};
