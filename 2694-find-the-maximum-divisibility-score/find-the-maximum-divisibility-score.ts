f382cabdb6f90916f5df3609f9e4c2a3fade6f59
Ilham Wahabi <wahabiputra@gmail.com>
2024-04-05 18:00:34 +0700

    Time: 243 ms (84.21%) | Memory: 54.6 MB (100.00%) - LeetSync

---
+++ 2694-find-the-maximum-divisibility-score/find-the-maximum-divisibility-score.ts
@@ -0,0 +1,23 @@
+function maxDivScore(nums: number[], divisors: number[]): number {
+    let max = -Infinity
+    let divisor = Infinity
+    
+    for (let i = 0; i < divisors.length; i++) {
+        let score = 0
+        
+        for (let j = 0; j < nums.length; j++) {
+            if (nums[j] % divisors[i] === 0) {
+                score++
+            }
+        }
+
+        if (score > max || (score === max && divisors[i] < divisor)) {
+            max = score
+            divisor = divisors[i]
+        }
+    }
+
+    console.log(max, divisor)
+
+    return divisor
+};
