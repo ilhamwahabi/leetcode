5c3b346c90e60b1f9e10e11fa03d3425b7d4ba35
Ilham Wahabi <wahabiputra@gmail.com>
2024-04-15 08:59:57 +0700

    Time: 47 ms (97.34%) | Memory: 51.7 MB (81.91%) - LeetSync

---
+++ 129-sum-root-to-leaf-numbers/sum-root-to-leaf-numbers.ts
@@ -0,0 +1,31 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+function sumNumbers(root: TreeNode | null): number {
+    function traverse(root: TreeNode | null, prev = "") {
+        if (root.left === null && root.right === null) {
+            return prev + String(root.val)
+        }
+        if (root.left !== null && root.right === null) {
+            return parseInt(traverse(root.left, prev + String(root.val)))
+        } else if (root.left === null && root.right !== null) {
+            return parseInt(traverse(root.right, prev + String(root.val)))
+        } else {
+            return parseInt(traverse(root.left, prev + String(root.val))) +
+                parseInt(traverse(root.right, prev + String(root.val)))
+        }
+    }
+
+    return parseInt(traverse(root))
+};
