890d6196b30af644930b58eebdf2ec737b4bb0e8
Ilham Wahabi <wahabiputra@gmail.com>
2024-05-01 15:16:37 +0700

    Time: 63 ms (14.41%) | Memory: 51 MB (90.68%) - LeetSync

--- 2128-reverse-prefix-of-word/reverse-prefix-of-word.ts
+++ 2128-reverse-prefix-of-word/reverse-prefix-of-word.ts
@@ -4,15 +4,11 @@ function reversePrefix(word: string, ch: string): string {
 
     let i = 0
     while (i < word.length && foundIndex === -1) {
-        if (foundIndex === -1) {
-            if (word[i] === ch) {
-                foundIndex = i
-                reversed = word[i] + reversed + word.slice(i + 1)
-            } else {
-                reversed = word[i] + reversed
-            }
+        if (word[i] === ch) {
+            foundIndex = i
+            reversed = word[i] + reversed + word.slice(i + 1)
         } else {
-            reversed = reversed + word[i]
+            reversed = word[i] + reversed
         }
         i++
     }
