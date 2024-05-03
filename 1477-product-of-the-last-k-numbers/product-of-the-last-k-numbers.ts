976aa83a51647b911b90c5daff88205e545b7fe6
Ilham Wahabi <wahabiputra@gmail.com>
2024-05-03 17:15:14 +0700

    Time: 239 ms (85.71%) | Memory: 78.9 MB (64.29%) - LeetSync

--- 1477-product-of-the-last-k-numbers/product-of-the-last-k-numbers.ts
+++ 1477-product-of-the-last-k-numbers/product-of-the-last-k-numbers.ts
@@ -1,14 +1,16 @@
 class ProductOfNumbers {
-    stream = []
+    stream = [1];
+    // add: [1] + 2 -> [1,2*1] + 3 -> [1,2*1,2*1*3] + 0 -> [1,2*1,2*1*3,2*1*3*0]
+    // getProduct: k = 1 -> 2*1*3 / 2*1 = 3, k = 2 -> 2*1*3 / 1 = 2*3
 
     add(num: number): void {
-        this.stream.push(num)
+        if (num === 0) this.stream = [1];
+        else this.stream.push(this.stream[this.stream.length - 1] * num);
     }
 
     getProduct(k: number): number {
-        return this.stream
-            .slice(this.stream.length - k, this.stream.length)
-            .reduce((acc, curr) => acc * curr, 1)
+        if (k >= this.stream.length) return 0; // that mean we found 0 before
+        return this.stream[this.stream.length - 1] / this.stream[this.stream.length - k - 1];
     }
 }
 
