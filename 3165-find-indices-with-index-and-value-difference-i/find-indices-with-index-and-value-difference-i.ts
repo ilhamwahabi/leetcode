63acd5c847dbca51d97ae5d8b5bda9589cee0516
Ilham Wahabi <wahabiputra@gmail.com>
2024-05-04 14:50:06 +0700

    Time: 75 ms (8.70%) | Memory: 52.2 MB (17.39%) - LeetSync

--- 3165-find-indices-with-index-and-value-difference-i/find-indices-with-index-and-value-difference-i.ts
+++ 3165-find-indices-with-index-and-value-difference-i/find-indices-with-index-and-value-difference-i.ts
@@ -1,7 +1,7 @@
 function findIndices(nums: number[], indexDifference: number, valueDifference: number): number[] {
     for (let i = 0; i < nums.length; i++) {
-        for (let j = 0; j < nums.length; j++) {
-            if (Math.abs(i-j) >= indexDifference && Math.abs(nums[i]-nums[j]) >= valueDifference) {
+        for (let j = i + indexDifference; j < nums.length; j++) {
+            if (Math.abs(nums[i]-nums[j]) >= valueDifference) {
                 return [i,j]
             }
         }
