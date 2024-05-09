c4f0d339bed5d401a976f7023c6cd20b1e842345
Ilham Wahabi <wahabiputra@gmail.com>
2024-05-10 00:53:46 +0700

    Time: 53 ms (88.89%) | Memory: 50.4 MB (77.78%) - LeetSync

--- 2154-minimum-moves-to-convert-string/minimum-moves-to-convert-string.ts
+++ 2154-minimum-moves-to-convert-string/minimum-moves-to-convert-string.ts
@@ -2,9 +2,8 @@ function minimumMoves(s: string): number {
     let step = 0
 
     let i = 0
-    let str = s
-    while (i < str.length) {
-        if (str[i] === "X") {
+    while (i < s.length) {
+        if (s[i] === "X") {
             step++
             i = i + 3
         } else {
