ed6821ebab3bef898630dffce6fd8ff42e619b33
Ilham Wahabi <wahabiputra@gmail.com>
2024-05-12 21:39:54 +0700

    Time: 65 ms (35.48%) | Memory: 51 MB (74.19%) - LeetSync

--- 1915-check-if-one-string-swap-can-make-strings-equal/check-if-one-string-swap-can-make-strings-equal.ts
+++ 1915-check-if-one-string-swap-can-make-strings-equal/check-if-one-string-swap-can-make-strings-equal.ts
@@ -5,9 +5,11 @@ function areAlmostEqual(s1: string, s2: string): boolean {
 
     const diff = []
     for (let i = 0; i < s1.length; i++) {
-        if (s1[i] !== s2[i]) diff.push(i)
+        if (s1[i] !== s2[i]) {
+            diff.push(i)
+            if (diff.length > 2) return false
+        }
     }
 
-    if (diff.length > 2) return false
     return s1[diff[0]] === s2[diff[1]] && s1[diff[1]] === s2[diff[0]]
 };
