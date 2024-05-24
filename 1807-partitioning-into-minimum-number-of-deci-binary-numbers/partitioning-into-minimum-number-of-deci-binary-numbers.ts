b5d94704453cb0477990501c28afe174f5e999f0
Ilham Wahabi <wahabiputra@gmail.com>
2024-05-24 19:04:49 +0700

    Time: 84 ms (40.59%) | Memory: 55.2 MB (63.37%) - LeetSync

--- 1807-partitioning-into-minimum-number-of-deci-binary-numbers/partitioning-into-minimum-number-of-deci-binary-numbers.ts
+++ 1807-partitioning-into-minimum-number-of-deci-binary-numbers/partitioning-into-minimum-number-of-deci-binary-numbers.ts
@@ -1,9 +1,15 @@
 function minPartitions(n: string): number {
     // Find max digit
 
+    let i = 0
+    let nine = false
     let max = "0"
-    for (const num of n) {
-        if (num > max) max = num
+
+    while (i < n.length && !nine) {
+        if (n[i] > max) max = n[i]
+        if (n[i] === "9") nine = true
+        i++
     }
-    return parseInt(max)
+    
+    return nine ? 9 : parseInt(max)
 };
