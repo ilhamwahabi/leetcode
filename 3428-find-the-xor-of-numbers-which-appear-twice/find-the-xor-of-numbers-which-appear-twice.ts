70477125a5b5d67720d02a10246a0b1e3f05c0d6
Ilham Wahabi <wahabiputra@gmail.com>
2024-05-31 19:12:38 +0700

    Time: 62 ms (78.57%) | Memory: 51.8 MB (82.86%) - LeetSync

--- 3428-find-the-xor-of-numbers-which-appear-twice/find-the-xor-of-numbers-which-appear-twice.ts
+++ 3428-find-the-xor-of-numbers-which-appear-twice/find-the-xor-of-numbers-which-appear-twice.ts
@@ -2,14 +2,13 @@ function duplicateNumbersXOR(nums: number[]): number {
     // Store occurances in map
     // Iterate map, check the element that appear twice, get XOR
 
+    let result = 0
+
     const map = {}
     nums.forEach(num => {
         map[num] ? map[num]++ : map[num] = 1
+        if (map[num] === 2) result ^= num
     })
 
-    let result = 0
-    for (const key in map) {
-        if (map[key] === 2) result = result ^ +key
-    }
     return result
 };
