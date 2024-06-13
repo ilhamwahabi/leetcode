18d630508aec396fa15b3465e15daa6e457a6815
Ilham Wahabi <wahabiputra@gmail.com>
2024-06-13 18:41:05 +0700

    Time: 60 ms (33.33%) | Memory: 52.8 MB (11.11%) - LeetSync

---
+++ 1933-number-of-different-integers-in-a-string/number-of-different-integers-in-a-string.ts
@@ -0,0 +1,29 @@
+function numDifferentIntegers(word: string): number {
+    // Iterate word, find number: check using regex and if so store it
+    // At end store in map, parse it first to handle leading 0
+    // Return total map key
+
+    const map = {}
+
+    let num = ""
+    for (let i = -1; i < word.length + 1; i++) {
+        const curr = word[i]
+        const next = word[i+1]
+        
+        if (/[0-9]/.test(curr)) {
+            if (
+                !(curr === "0" && num === "") ||
+                curr === "0" && !/[0-9]/.test(next)
+            ) {
+                num += curr
+            }
+        } else {
+            if (num.length > 0) {
+                map[num] = true
+                num = ""
+            }
+        }
+    }
+ 
+    return Object.keys(map).length
+};
