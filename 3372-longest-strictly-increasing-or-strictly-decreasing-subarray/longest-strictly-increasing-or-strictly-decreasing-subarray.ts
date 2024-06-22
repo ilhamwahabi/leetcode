202c725c4a06f0f7bfc3fbc5fcd18c3566a1e177
Ilham Wahabi <wahabiputra@gmail.com>
2024-06-22 15:13:05 +0700

    Time: 68 ms (60.00%) | Memory: 52.1 MB (44.00%) - LeetSync

--- 3372-longest-strictly-increasing-or-strictly-decreasing-subarray/longest-strictly-increasing-or-strictly-decreasing-subarray.ts
+++ 3372-longest-strictly-increasing-or-strictly-decreasing-subarray/longest-strictly-increasing-or-strictly-decreasing-subarray.ts
@@ -2,27 +2,21 @@ function longestMonotonicSubarray(nums: number[]): number {
     let longest = 1
 
     let inc = 1
+    let dec = 1
     for (let i = 1; i < nums.length; i++) {
         const curr = nums[i]
         const prev = nums[i - 1]
 
         if (curr > prev) {
             inc++
+            dec = 1
             if (inc > longest) longest = inc
-        } else {
-            inc = 1
-        }
-    }
-
-    let dec = 1
-    for (let i = 1; i < nums.length; i++) {
-        const curr = nums[i]
-        const prev = nums[i - 1]
-
-        if (curr < prev) {
+        } else if (curr < prev) {
             dec++
+            inc = 1
             if (dec > longest) longest = dec
         } else {
+            inc = 1
             dec = 1
         }
     }
