68c13209ed49518a68a58579e6b4103ca99fbc38
Ilham Wahabi <wahabiputra@gmail.com>
2024-07-06 17:59:14 +0700

    Time: 45 ms (48.62%) | Memory: 16.6 MB (35.52%) - LeetSync

---
+++ 2723-find-the-longest-balanced-substring-of-a-binary-string/find-the-longest-balanced-substring-of-a-binary-string.py
@@ -0,0 +1,23 @@
+class Solution:
+    def findTheLongestBalancedSubstring(self, s: str) -> int:
+        # If we found 0, count it
+        # Then if we found 1, count it
+        # If we found 0 again or end of str, save max with min value between 0's and 1's
+        res = 0
+        zero = 0
+        one = 0
+        for i in range(len(s)):
+            prev = s[i-1]
+            curr = s[i]
+
+            if curr == "0":
+                if prev == "1":
+                    zero = 1
+                    one = 0
+                else:
+                    zero += 1
+            else:
+                one += 1
+                res = max(res, min(zero, one) * 2)
+        
+        return res
