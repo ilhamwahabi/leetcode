546d383386308b9773d0ff29560ab2b27892198d
Ilham Wahabi <wahabiputra@gmail.com>
2024-07-16 11:45:41 +0700

    Time: 41 ms (37.66%) | Memory: 16.4 MB (98.51%) - LeetSync

--- 3484-lexicographically-smallest-string-after-a-swap/lexicographically-smallest-string-after-a-swap.py
+++ 3484-lexicographically-smallest-string-after-a-swap/lexicographically-smallest-string-after-a-swap.py
@@ -2,10 +2,8 @@ class Solution:
     def getSmallestString(self, s: str) -> str:
         # Iterate s, if s[i] same parity with s[i-1] then swap it, check the min
 
-        mn = s
-
         for i in range(1, len(s)):
-            if (int(s[i]) + int(s[i-1])) % 2 == 0:
-                mn = min(mn, s[:i-1] + s[i] + s[i-1] + s[i+1:])
+            if (int(s[i]) + int(s[i-1])) % 2 == 0 and s[i] < s[i-1]:
+                return s[:i-1] + s[i] + s[i-1] + s[i+1:]
         
-        return mn
+        return s
