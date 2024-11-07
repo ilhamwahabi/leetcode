5b0749a4051390cae20b8f0af2e35b2475e9c452
Ilham Wahabi <wahabiputra@gmail.com>
2024-11-07 22:00:03 +0700

    Time: 118 ms (100.00%) | Memory: 63.7 MB (100.00%) - LeetSync

--- 2356-largest-combination-with-bitwise-and-greater-than-zero/largest-combination-with-bitwise-and-greater-than-zero.ts
+++ 2356-largest-combination-with-bitwise-and-greater-than-zero/largest-combination-with-bitwise-and-greater-than-zero.ts
@@ -1,19 +1,16 @@
 function largestCombination(candidates: number[]): number {
-    const map = Array.from({ length: 24 }, () => 0)
+    const map = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
 
+    let max = 0
     for (const candidate of candidates) {
         const binary = candidate.toString(2)
 
         for (let i = 0; i < binary.length; i++) {
             if (binary[i] === '1') {
                 map[binary.length - 1 - i]++
+                max = Math.max(map[binary.length - 1 - i], max)
             }
         }
-    }
-
-    let max = 0
-    for (let i = 0; i < map.length; i++) {
-        if (map[i] > max) max = map[i]
     }
     return max
 };
