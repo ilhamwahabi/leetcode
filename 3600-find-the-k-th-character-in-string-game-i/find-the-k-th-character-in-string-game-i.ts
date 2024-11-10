70815bc55cbbedb3a5851a84eb532dee68c3c135
Ilham Wahabi <wahabiputra@gmail.com>
2024-11-10 20:23:48 +0700

    Time: 11 ms (15.38%) | Memory: 54.9 MB (22.22%) - LeetSync

--- 3600-find-the-k-th-character-in-string-game-i/find-the-k-th-character-in-string-game-i.ts
+++ 3600-find-the-k-th-character-in-string-game-i/find-the-k-th-character-in-string-game-i.ts
@@ -1,13 +1,17 @@
 function kthCharacter(k: number): string {
     let str = 'a'
+    const map = {
+        'a': 'b', 'b': 'c', 'c': 'd', 'd': 'e', 'e': 'f', 
+        'f': 'g', 'g': 'h', 'h': 'i', 'i': 'j', 'j': 'k', 
+        'k': 'l', 'l': 'm', 'm': 'n', 'n': 'o', 'o': 'p', 
+        'p': 'q', 'q': 'r', 'r': 's', 's': 't', 't': 'u', 
+        'u': 'v', 'v': 'w', 'w': 'x', 'x': 'y', 'y': 'z', 
+        'z': 'a'
+    };
 
     while (str.length < k) {
         let append = ''
-        for (const char of str) {
-            if (char === 'z') append += 'a'
-            else append += String.fromCharCode(char.charCodeAt(0) + 1)
-        }
-
+        for (const char of str) append += map[char]
         str += append
     }
 
