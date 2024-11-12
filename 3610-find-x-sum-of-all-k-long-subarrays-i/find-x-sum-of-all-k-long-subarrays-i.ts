805ab89ee7ac3292d7152488707dfb65a3d63827
Ilham Wahabi <wahabiputra@gmail.com>
2024-11-13 06:39:27 +0700

    Time: 26 ms (27.59%) | Memory: 57.7 MB (77.91%) - LeetSync

---
+++ 3610-find-x-sum-of-all-k-long-subarrays-i/find-x-sum-of-all-k-long-subarrays-i.ts
@@ -0,0 +1,33 @@
+function findXSum(nums: number[], k: number, x: number): number[] {
+    const result = []
+    const map = {}
+
+    for (let i = 0; i < k; i++) {
+        map[nums[i]] ? map[nums[i]]++ : map[nums[i]] = 1
+    }
+    const sum = countSum(map, x)
+    result.push(sum)
+
+    for (let i = 1; i <= nums.length - k; i++) {
+        map[nums[i-1]]--
+        if (map[nums[i-1]] === 0) delete map[nums[i-1]]
+        
+        map[nums[i+k-1]] ? map[nums[i+k-1]]++ : map[nums[i+k-1]] = 1
+
+        const sum = countSum(map, x)
+        result.push(sum)
+    }
+
+    return result
+};
+
+function countSum(map, x) {
+    return Object.keys(map)
+        // @ts-ignore
+        .toSorted((a,b) => {
+            if (map[a] === map[b]) return +b - +a
+            return map[b] - map[a]
+        })
+        .slice(0, x)
+        .reduce((acc, curr) => acc + curr * map[curr], 0)
+}
