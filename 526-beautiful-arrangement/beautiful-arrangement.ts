65d193f46af5a9baeeeac7a457b9fd27b3dfa208
Ilham Wahabi <wahabiputra@gmail.com>
2025-04-26 14:52:16 +0700

    Time: 161 ms (19.35%) | Memory: 61.4 MB (51.61%) - LeetSync

--- 526-beautiful-arrangement/beautiful-arrangement.ts
+++ 526-beautiful-arrangement/beautiful-arrangement.ts
@@ -1,22 +1,23 @@
 function countArrangement(n: number): number {
     // Backtracking
-    
+
+    const init = Array.from({ length: n }, (_, i) => i + 1)    
     let count = 0
 
-    function backtracking(arr = [], set = new Set()) {
+    function backtracking(arr = [], set = new Set(init)) {
         if (arr.length === n) {
             count++
         } else {
-            for (let i = 1; i <= n; i++) {
-                const cond1 = i % (arr.length + 1) === 0
-                const cond2 = (arr.length + 1) % i === 0
+            for (const value of set) {
+                const cond1 = value % (arr.length + 1) === 0
+                const cond2 = (arr.length + 1) % value === 0
 
-                if (!set.has(i) && (cond1 || cond2)) {
+                if (cond1 || cond2) {
                     const copy = new Set(set)
-                    copy.add(i)
+                    copy.delete(value)
 
-                    backtracking([...arr, i], copy)
-                }
+                    backtracking([...arr, value], copy)
+                }   
             }
         }
     }
