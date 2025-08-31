ed9d8a4b61e807bf914f0eab260f91f05e61095c
Ilham Wahabi <wahabiputra@gmail.com>
2025-09-01 00:40:54 +0700

    Time: 2 ms (50.00%) | Memory: 64.8 MB (85.00%) - LeetSync

--- 3973-flip-square-submatrix-vertically/flip-square-submatrix-vertically.ts
+++ 3973-flip-square-submatrix-vertically/flip-square-submatrix-vertically.ts
@@ -1,11 +1,9 @@
 function reverseSubmatrix(grid: number[][], x: number, y: number, k: number): number[][] {
-    // Start from row x and column y, iterate y until k, then x until <= Math.floor((grid.length - 1 - x) / 2)
-    // Swap each element in x and (grid.length - 1) - currRow - x
+    // Start from row x and column y, iterate y until k, then x until < Math.floor(k / 2)
+    // Swap each element in x and (swapLength - 1) - (currRow - x)
 
     // Time: O(n ^ n), iterate row and cell in matrix
     // Space: O(1), only store constant
-
-    const matrixHeight = grid.length - 1
 
     for (let i = x; i < x + Math.floor(k / 2); i++) {
         for (let j = y; j < y + k; j++) {
