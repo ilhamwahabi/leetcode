3470d0b2aa372fd147ebff7335c074800a7bfb99
Ilham Wahabi <wahabiputra@gmail.com>
2025-08-31 21:28:00 +0700

    Time: 281 ms (9.02%) | Memory: 57.6 MB (51.64%) - LeetSync

--- 3995-gcd-of-odd-and-even-sums/gcd-of-odd-and-even-sums.ts
+++ 3995-gcd-of-odd-and-even-sums/gcd-of-odd-and-even-sums.ts
@@ -2,16 +2,11 @@ function gcdOfOddEvenSums(n: number): number {
     // 16 = 1 2 4 8 16
     // 20 = 1 2 4 5 10 20
 
-    // Sum the first n odd number, start from 1
-    // Sum the first n even number, start from 1
-    // Find the factorial for each of them, start from 1 to x/2, save in set
-    // Iterate from odd x from the largest, return
-
     // Time: O(n), iterate number and fact
-    // Space: O(n), storing facts
+    // Space: O(1), storing const only
 
-    let odd = n * (n + 0)
-    let even = n * (n + 1)
+    const odd = n * (n + 0)
+    const even = n * (n + 1)
 
     for (let i = Math.floor(odd / 2); i > 0; i--) {
         if (odd % i === 0 && even % i === 0) return i
