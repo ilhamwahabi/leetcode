f417932a1a33b4ddffb45009686980795b5ad2ae
Ilham Wahabi <wahabiputra@gmail.com>
2025-09-02 14:03:09 +0700

    Time: 5 ms (66.00%) | Memory: 65.2 MB (84.00%) - LeetSync

--- 3931-process-string-with-special-operations-i/process-string-with-special-operations-i.ts
+++ 3931-process-string-with-special-operations-i/process-string-with-special-operations-i.ts
@@ -2,19 +2,26 @@ function processStr(s: string): string {
     // Time: O(n ^ n)
     // Space: O(n)
 
-    let result = []
+    let result = ""
+    let reverse = ""
 
     for (const char of s) {
         if (char === "*") {
-            result.pop()
+            result = result.slice(0, result.length - 1)
+            reverse = reverse.slice(1)
         } else if (char === "#") {
-            result.push(...result)
+            result += result
+            reverse += reverse
         } else if (char === "%") {
-            result.reverse()
+            const temp = result
+
+            result = reverse
+            reverse = temp
         } else {
-            result.push(char)
+            result = result + char
+            reverse = char + reverse
         }
     }
 
-    return result.join("")
+    return result
 };
