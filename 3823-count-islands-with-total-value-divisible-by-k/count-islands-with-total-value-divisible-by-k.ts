1e7737634a3f38f79d4ea7c19b5518e71912b0f4
Ilham Wahabi <wahabiputra@gmail.com>
2025-09-02 14:47:22 +0700

    Time: 296 ms (45.28%) | Memory: 79.1 MB (79.25%) - LeetSync

--- 3823-count-islands-with-total-value-divisible-by-k/count-islands-with-total-value-divisible-by-k.ts
+++ 3823-count-islands-with-total-value-divisible-by-k/count-islands-with-total-value-divisible-by-k.ts
@@ -1,12 +1,11 @@
 function countIslands(grid: number[][], k: number): number {
-    const visited = new Set()
     let total = 0
 
     for (let i = 0; i < grid.length; i++) {
         for (let j = 0; j < grid[i].length; j++) {
             const cell = grid[i][j]
 
-            const sum = dfs(grid, i, j, visited)
+            const sum = dfs(grid, i, j)
             if (sum !== 0 && sum % k === 0) {
                 total++
             }
@@ -16,15 +15,15 @@ function countIslands(grid: number[][], k: number): number {
     return total
 };
 
-function dfs(grid: number[][], i: number, j: number, visited): number {
+function dfs(grid: number[][], i: number, j: number): number {
     if (!grid?.[i]?.[j]) return 0
-    if (visited.has(`${i}-${j}`)) return 0
 
-    visited.add(`${i}-${j}`)
+    const value = grid[i][j]
+    grid[i][j] = 0
 
-    return grid?.[i]?.[j]
-        + dfs(grid, i + 1, j, visited)
-        + dfs(grid, i, j + 1, visited)
-        + dfs(grid, i - 1, j, visited)
-        + dfs(grid, i, j - 1, visited)
+    return value
+        + dfs(grid, i + 1, j)
+        + dfs(grid, i, j + 1)
+        + dfs(grid, i - 1, j)
+        + dfs(grid, i, j - 1)
 }
