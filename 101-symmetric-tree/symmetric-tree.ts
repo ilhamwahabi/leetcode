585f4d8bd500d6972fc6851100b7c432d89abc22
Ilham Wahabi <wahabiputra@gmail.com>
2025-09-05 17:22:09 +0700

    Time: 11 ms (3.89%) | Memory: 59 MB (5.45%) - LeetSync

---
+++ 101-symmetric-tree/symmetric-tree.ts
@@ -0,0 +1,42 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+function isSymmetric(root: TreeNode | null): boolean {
+    let left = ""
+    let right = ""
+    function recursion(root: TreeNode | null, direction: "left" | "right") {
+        if (!root) {
+            if (direction === "left") left += "null"
+            if (direction === "right") right += "null"
+            return
+        }
+
+        if (direction === "left") {
+            left += root.val
+            recursion(root.left, direction)
+            recursion(root.right, direction)
+        }
+        if (direction === "right") {
+            right += root.val
+            recursion(root.right, direction)
+            recursion(root.left, direction)
+        }
+    }
+    recursion(root.left, "left")
+    recursion(root.right, "right")
+
+    console.log({ left, right })
+
+    return left === right
+};
