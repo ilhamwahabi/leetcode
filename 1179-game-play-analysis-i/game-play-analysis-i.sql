7475f59c60e48f7ed16e89c337b41e7afa4ffaec
Ilham Wahabi <wahabiputra@gmail.com>
2025-12-05 23:09:57 +0700

    Time: 443 ms (84.53%) | Memory: 0B (100.00%) - LeetSync

--- 1179-game-play-analysis-i/game-play-analysis-i.sql
+++ 1179-game-play-analysis-i/game-play-analysis-i.sql
@@ -1,5 +1,4 @@
 # Write your MySQL query statement below
-SELECT player_id, MIN(event_date) AS first_login
+SELECT DISTINCT player_id, FIRST_VALUE(event_date) OVER (PARTITION BY player_id ORDER BY event_date) AS first_login
 FROM Activity
-GROUP BY player_id
 ;
