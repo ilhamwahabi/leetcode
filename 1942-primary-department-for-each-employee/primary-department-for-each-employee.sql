5ecec0429e3dbcec4f090abe88c358b134203361
Ilham Wahabi <wahabiputra@gmail.com>
2025-12-06 23:53:35 +0700

    Time: 502 ms (70.75%) | Memory: 0B (100.00%) - LeetSync

--- 1942-primary-department-for-each-employee/primary-department-for-each-employee.sql
+++ 1942-primary-department-for-each-employee/primary-department-for-each-employee.sql
@@ -1,14 +1,9 @@
 # Write your MySQL query statement below
-(
-    SELECT employee_id, department_id
+WITH temp AS (
+    SELECT *, COUNT(primary_flag) OVER (PARTITION BY employee_id) AS flag_count
     FROM Employee
-    WHERE primary_flag = 'Y'
 )
-UNION
-(
-    SELECT employee_id, department_id
-    FROM Employee
-    GROUP BY employee_id
-    HAVING COUNT(employee_id) = 1
-)
+SELECT employee_id, department_id
+FROM temp
+WHERE flag_count = 1 OR primary_flag = 'Y'
 ;
