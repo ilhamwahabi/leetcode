a5a5da6c773184e6a5794bd107631a5003193152
Ilham Wahabi <wahabiputra@gmail.com>
2025-12-13 00:25:38 +0700

    Time: 503 ms (39.91%) | Memory: 0B (100.00%) - LeetSync

--- 1523-capital-gainloss/capital-gainloss.sql
+++ 1523-capital-gainloss/capital-gainloss.sql
@@ -1,14 +1,10 @@
 # Write your MySQL query statement below
-SELECT stock_name, SUM(capital_gain_loss) AS capital_gain_loss FROM ((
-    SELECT stock_name, SUM(price) * -1 AS capital_gain_loss
-    FROM Stocks
-    WHERE operation = "Buy"
-    GROUP BY stock_name
-) UNION (
-    SELECT stock_name, SUM(price) AS capital_gain_loss
-    FROM Stocks
-    WHERE operation = "Sell"
-    GROUP BY stock_name
-)) AS temp
+SELECT stock_name, SUM(
+    CASE
+        WHEN operation = 'BUY' THEN -price
+        ELSE price
+    END
+) AS capital_gain_loss
+FROM Stocks
 GROUP BY stock_name
 ;
