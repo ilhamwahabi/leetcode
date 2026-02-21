7782f5e6ba001f6803619fd059d9948e1d5acfcb
Ilham Wahabi <wahabiputra@gmail.com>
2026-02-21 20:22:03 +0700

    Time: 0 ms (100.00%) | Memory: 59.7 MB (35.00%) - LeetSync

--- 2320-find-all-k-distant-indices-in-an-array/find-all-k-distant-indices-in-an-array.ts
+++ 2320-find-all-k-distant-indices-in-an-array/find-all-k-distant-indices-in-an-array.ts
@@ -1,20 +1,20 @@
 function findKDistantIndices(nums: number[], key: number, k: number): number[] {
-    // Iterate nums, search all key location
-    // Store all index in range [keyIndex - i ... i - keyIndex]
+    const keyIndexes = []
+    nums.forEach((num, index) => {
+        if (num === key) keyIndexes.push(index)
+    })
 
-    const result = new Set<number>()
-
+    let currentKeyIndexesIndex = 0
+    const result = []
     for (let i = 0; i < nums.length; i++) {
-        if (nums[i] === key) {
-            for (let j = Math.max(i - k, 0); j < i; j++) {
-                result.add(j)
-            }
-            result.add(i)
-            for (let j = i + 1; j <= Math.min(i + k, nums.length - 1); j++) {
-                result.add(j)
-            }
+        const num = nums[i]
+        const idx = keyIndexes[currentKeyIndexesIndex]
+
+        if (Math.abs(i - idx) <= k) {
+            result.push(i)
+            if (i - idx === k) currentKeyIndexesIndex++
         }
     }
-
-    return Array.from(result)
+    
+    return result
 };
