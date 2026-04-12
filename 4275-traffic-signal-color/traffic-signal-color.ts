10b606b2d578d70e121e43210965842951c3f388
Ilham Wahabi <wahabiputra@gmail.com>
2026-04-12 12:31:54 +0700

    Time: 0 ms (100.00%) | Memory: 57.9 MB (100.00%) - LeetSync

---
+++ 4275-traffic-signal-color/traffic-signal-color.ts
@@ -0,0 +1,6 @@
+function trafficSignal(timer: number): string {
+    if (timer === 0) return 'Green'
+    if (timer === 30) return 'Orange'
+    if (timer > 30 && timer <= 90) return 'Red'
+    return 'Invalid'
+};
