520904622e1c83fe2612d16c06c6ecd6a7cc161c
Ilham Wahabi <wahabiputra@gmail.com>
2026-06-15 13:06:44 +0700

    Time: 24 ms (31.78%) | Memory: 63.7 MB (26.17%) - LeetSync

--- 4313-password-strength/password-strength.ts
+++ 4313-password-strength/password-strength.ts
@@ -1,16 +1,11 @@
 function passwordStrength(password: string): number {
-    const set = new Set()
 
     let points = 0
-    for (const char of password) {
-        if (!set.has(char)) {
-            set.add(char)
-
-            if (char.match(/[a-z]/)) points += 1
-            else if (char.match(/[A-Z]/)) points += 2
-            else if (char.match(/[0-9]/)) points += 3
-            else if (char.match(/[!@#$]/)) points += 5
-        }
+    for (const char of new Set(password)) {
+        if (/[a-z]/.test(char)) points += 1
+        else if (/[A-Z]/.test(char)) points += 2
+        else if (/[0-9]/.test(char)) points += 3
+        else if (/[!@#$]/.test(char)) points += 5
     }
 
     return points
